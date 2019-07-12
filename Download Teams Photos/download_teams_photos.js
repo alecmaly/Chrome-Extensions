ee6adaa33af6d8d15eb5dace685dcc03f135b7d7
Alec Maly <alecjmaly@gmail.com>
2019-07-12 02:22:54 -0400

    fix teams extension

--- Download Teams Photos/download_teams_photos.js
+++ Download Teams Photos/download_teams_photos.js
@@ -6,6 +6,8 @@ window.onload = () => {
     // need to wait until loaded
     waitForElementToDisplay('.team-properties-container', 1000);
 
+
+
     function waitForElementToDisplay(selector, time) {
         if(document.querySelector(selector)!=null) {
             if(document.querySelector('#downloadImagesButton') == null) {
@@ -15,12 +17,35 @@ window.onload = () => {
                 btn.style.borderRadius = '5px';
                 btn.style.backgroundColor = 'lightgoldenrodyellow';
                 btn.style.fontWeight = '600';
-                btn.onclick = () => {
-                    var images = document.querySelectorAll('[itemtype="http://schema.skype.com/AMSImage"]');
-                    //downloadImage(images[0].src);
-                    Array.from(images).forEach(async image => {
-                        await downloadImage(image.src);
-                    });
+                btn.onclick = async () => {
+                    window.downloaded_images = [];
+                    window.failures = [];
+
+                    const downloadImagesFunction = async function() {
+                      // download images function
+                      var images = document.querySelectorAll('[itemtype="http://schema.skype.com/AMSImage"][src^="blob:"]');
+
+                      await asyncForEach(Array.from(images), async image => {
+                          // image has not been downloaded yet
+                            let img_name = image.attributes['target-src'].value.split('/')[5]
+                            if (!window.downloaded_images.includes(img_name)) {
+                              if (!image.src.includes('blob:')) {
+                                console.log(image);
+                                window.failures.push(image);
+                              }
+                              downloadImage(image.src, img_name);
+                              await sleep(200); // time to download image
+                              window.downloaded_images.push(img_name);
+                              console.log(window.downloaded_images.length);
+
+                            }
+
+
+                      });
+
+                      setTimeout(downloadImagesFunction, 200)
+                    }
+                    downloadImagesFunction();
                 };
                 document.querySelector('.team-properties-container').appendChild(btn);
             }
@@ -31,16 +56,29 @@ window.onload = () => {
     }
 
 
-
-    function downloadImage(uri) {
-        // create anchor tag and download
-        var link = document.createElement('a');
-        link.href = uri;
-        link.download = uri.split('/')[uri.split('/').length - 1] + '.jpg';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+    function downloadImage(uri, name) {// create anchor tag and download
+      let link = document.createElement('a');
+      link.href = uri;
+      link.download = name + '.jpg';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     }
+
+
+
+
+
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function asyncForEach(array, callback) {
+      for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+      }
+    }
+
 }
 
 
