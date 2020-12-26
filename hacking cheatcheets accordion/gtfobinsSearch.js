e2b337e3757e2a4d2cf0e1b3b40853405feaeb38
Alec Maly <alecjmaly@gmail.com>
2020-12-25 23:42:05 -0500

    udpated

--- hacking cheatcheets accordion/gtfobinsSearch.js
+++ hacking cheatcheets accordion/gtfobinsSearch.js
@@ -17,24 +17,34 @@ wrapper.appendChild(ele)
 
 
 
-
-
 function updateVisibility() {
   // get values to search for
-  var programs = document.querySelector('#myinput').value.split('\n').map(val => { return val.split('/').splice(-1)[0] }).filter(val => { return val != ""})
+  var programs = document.querySelector('#myinput').value.split('\n').map(val => { return val.split('/').splice(-1)[0].split('.')[0] }).filter(val => { return val != ""})
 
   // hide if not in programs
   var rows = document.querySelectorAll('tr')
   Array.from(rows).forEach(row => {
     let row_program = row.innerText.split('\n').map(val => { return val.trim() }).filter(val => { return val != "" })[0]
     
-    if (programs.includes(row_program) || programs.length == 0) 
+    /* update matched programs */
+    // if (programs.includes(row_program) || programs.length == 0) 
+    if (programs.filter(val => { return val.startsWith(row_program) }).length > 0 || programs.length == 0) {
       row.style.display = 'block'
-    else 
+        
+      if (row.querySelector('#matches') == null) {
+        // does not exist - create
+        var ele = document.createElement('td')
+        ele.id = 'matches'
+        ele.innerText = "(" + programs.filter(val => { return val.startsWith(row_program) }).join(',') + ")"
+        row.appendChild(ele)
+      } else {
+        row.querySelector('#matches').innerText = "(" + programs.filter(val => { return val.startsWith(row_program) }).join(',') + ")"
+      }
+
+    } else 
       row.style.display = 'none'
   })
 }
-
 
 
 
